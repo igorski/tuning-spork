586b257d6e449905dd5195b3a9335070c7a9ea05
Igor Zinken <igor.zinken@igorski.nl>
2020-11-29 17:05:20 +0100

    Application now also outputs self contained template

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+const dirSrc = `./src`;
+
 module.exports = {
-    filenameHashing: false,
-    productionSourceMap: false
+    //lintOnSave: false,
+    publicPath: './',
+    productionSourceMap: false,
+    pages: {
+        // self contained page
+        index: {
+            entry: `${dirSrc}/main.js`,
+            template: 'public/index.html',
+        },
+        // application assets only (e.g. HTML <body />)
+        app: {
+            entry: `${dirSrc}/main.js`,
+            template: 'public/index-app.html',
+        }
+    }
 };
